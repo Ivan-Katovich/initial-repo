d65f929c3b120d60658ad3f52f7b52be4d3d8ce5
Ivan Kotovich <kotovich.ivan@gmail.com>
2021-09-18 12:43:17 +0400

    444

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const date = require('./date');
 function foo() {
     const dt = date.bar('hh:mm DD/MM/YY');
     // date.bar
-    console.log('---------------111112222');
+    console.log('---------------111112222444');
     console.log(dt, date.b);
 }
 
